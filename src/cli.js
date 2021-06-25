db355770dce059c867525b94452aea5994af256d
oldrefery <35261486+oldrefery@users.noreply.github.com>
2021-06-25 09:17:51 +0300

    refactor: Fix greeting text

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,7 @@ import getAnswer from './utils/get-answer.js';
 const greetingAndGetUserName = () => {
   console.log('Welcome to the Brain Games!');
   const name = getAnswer('May I have your name? ');
-  console.log(`Hi, ${name}!`);
+  console.log(`Hello, ${name}!`);
   return name;
 };
 
